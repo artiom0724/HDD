34a26910b305f614a6cdb973b178a69386a1113b
artiom0724 <artiom0724@gmail.com>
2017-10-12 23:37:34 +0300

    Sizes

--- HDDinfo.py
+++ HDDinfo.py
@@ -11,6 +11,19 @@ dma = re.compile('(?<=DMA modes: {2}).*')
 udma = re.compile('(?<=UDMA modes: ).*')
 pio = re.compile('(?<=PIO modes: {2}).*')
 
+total_space_result = subprocess.check_output(['hdparm', '-I', "/dev/sda"])
+total_size_regex = re.compile('(?<=\tdevice size with M = 1024\*1024: {6}).*(?= MBytes)')
+total_size = int(total_size_regex.findall(total_space_result.decode())[0])
+
+
+free_space_result = subprocess.Popen(shlex.split('df -m'), stdout=subprocess.PIPE)
+searched = subprocess.Popen(shlex.split('grep ' + "/dev/sda"), stdin=free_space_result.stdout, stdout=subprocess.PIPE)
+awked = subprocess.Popen(shlex.split('awk \'{print $4}\''), stdin=searched.stdout, stdout=subprocess.PIPE)
+out = awked.communicate()
+total_free = 0
+for free in out[0].decode().split():
+    total_free += int(free)
+
 print('Model: ' + model.findall(result.decode())[0] + '\n')
 print('Firmware Revision: ' + firmware.findall(result.decode())[0] + '\n')
 print('Serial Number: ' + serial_num.findall(result.decode())[0] + '\n')
@@ -18,4 +31,6 @@ print('Supported ATA Interfaces: ' + ata_interfaces.findall(result.decode())[0] 
 print('DMA: ' + dma.findall(result.decode())[0] + '\n')
 print('UDMA: ' + udma.findall(result.decode())[0] + '\n* (signifies the current active mode)\n')
 print('PIO: ' + pio.findall(result.decode())[0] + '\n')
-
+print("Total space: " + str(total_size) + ' MBytes')
+print('Used and unavailable to access from this system: ' + str(total_size - total_free) + " MBytes")
+print('Free: ' + str(total_free) + " MBytes")
